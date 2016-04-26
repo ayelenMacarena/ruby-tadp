77099ca5d8e407cf329d9f3fffa4f08b26cb6896
Leandro Narosky <leandro.narosky@despegar.com>
2016-04-26 10:27:47 -0300

    Agregado el de listas

--- Tp-Ruby/src/patternMaching.rb
+++ Tp-Ruby/src/patternMaching.rb
@@ -9,7 +9,21 @@ class Object
     Proc.new{|x| x.class.ancestors.include?(unTipo)}
   end
 
+  def list(anArray, compare_size=true)
+    Proc.new{|x|
+      size = anArray.size;
+      valid = x[0..size-1] == anArray;
+      if not compare_size
+        valid
+      else
+        (size==(x.size)) &&valid
+      end
+
+    }
   end
+
+end
+
 
 #  def define_singleton_method(*args, &block){
 #
