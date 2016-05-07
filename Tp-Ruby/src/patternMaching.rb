d2931c7b8cd0c69c8fb511c086d51f53186f0fed
Leandro Narosky <leandro.narosky@despegar.com>
2016-05-07 19:14:32 -0300

    Codigo reidentado - Ese codigo si que se deja leer

--- Tp-Ruby/src/patternMaching.rb
+++ Tp-Ruby/src/patternMaching.rb
@@ -1,9 +1,9 @@
-
 class Entorno
-  def setea(symbol,valor)
+  def setea(symbol, valor)
     self.singleton_class.send(:attr_accessor, symbol)
-    self.send("#{symbol}=".to_sym,valor)
+    self.send("#{symbol}=".to_sym, valor)
   end
+
   def ejecuta(&block)
     instance_exec(&block)
   end
@@ -21,70 +21,70 @@ class Matching
   end
 
   def val(parametro)
-    Evaluation.new{|x| x == parametro}
+    Evaluation.new { |x| x == parametro }
   end
 
   def type(unTipo)
-    Evaluation.new{|x| x.class.ancestors.include?(unTipo)}
+    Evaluation.new { |x| x.class.ancestors.include?(unTipo) }
   end
 
   def duck(*methods)
-    Evaluation.new{|x| methods.all?{|m| x.methods.include?(m)}}
+    Evaluation.new { |x| methods.all? { |m| x.methods.include?(m) } }
   end
 
   def list(anArray, compare_size=true)
-    Proc.new{|x|
+    Proc.new { |x|
 
-        evaluations = []
-        list = []
-        if compare_size
-          anArray.size == x.size
-          list = x
+      evaluations = []
+      list = []
+      if compare_size
+        anArray.size == x.size
+        list = x
+      else
+        list = x[0..anArray.size-1]
+      end
+
+      anArray.zip(x) { |item1, item2|
+        if item1.is_a?(Symbol)
+          evaluations << item1.call(item2)
         else
-          list = x[0..anArray.size-1]
+          if val(item1).call(item2)
+            raise NoMatchingFoundException
+          end
         end
 
-        anArray.zip(x){|item1, item2|
-          if item1.is_a?(Symbol)
-            evaluations << item1.call(item2)
-          else
-            if val(item1).call(item2)
-              raise NoMatchingFoundException
-            end
-          end
-
       }
-        Evaluation.new{evaluations.each{|evaluation| instance_exec(&evaluation) }}
-        }
+      Evaluation.new { evaluations.each { |evaluation| instance_exec(&evaluation) } }
+    }
   end
 
   def with(*matchers, &block)
 
-    self.evaluations<< Evaluation.new{|x|
-      if (Evaluation.new{|x| true}.and(*matchers).call(x))
+    self.evaluations<< Evaluation.new { |x|
+      if (Evaluation.new { |x| true }.and(*matchers).call(x))
+        matchers.select { |match| match.call(x)!=true }.each { |evaluation|
+          instance_exec(&(evaluation.call(x))) }
+        instance_exec(&block)
+        true
+      else
+        false
+      end
 
-                                                matchers.select{|match| match.call(x)!=true}.each{|evaluation|
-                                                        instance_exec(&(evaluation.call(x)))}
-                                                        instance_exec(&block)
-                                                          true
-                              else
-                              false
-                              end
 
-
-  }
+    }
 
   end
 
   def otherwise (&block)
-    Evaluation.new{|x| e1.ejecuta(&block)}
+    Evaluation.new { |x| e1.ejecuta(&block) }
   end
 
   def matches?(algo, &bloque)
     instance_exec(&bloque)
-    self.evaluations.each{ |evaluation| if(evaluation.call(algo))
-                                        break
-                                        end
+    self.evaluations.each { |evaluation|
+      if (evaluation.call(algo))
+        break
+      end
 
     }
     self.evaluations.clear
@@ -97,15 +97,15 @@ class Evaluation < Proc
 
 
   def and(*otherEvaluations)
-    Evaluation.new{|x| self.call(x) && otherEvaluations.all?{|eval| eval.call(x)}}
+    Evaluation.new { |x| self.call(x) && otherEvaluations.all? { |eval| eval.call(x) } }
   end
 
   def or(*evaluations)
-    Evaluation.new{|x| self.call(x) || evaluations.any?{|eval| eval.call(x)}}
+    Evaluation.new { |x| self.call(x) || evaluations.any? { |eval| eval.call(x) } }
   end
 
   def not
-    Evaluation.new{|x| not self.call(x)}
+    Evaluation.new { |x| not self.call(x) }
   end
 
 end
@@ -114,9 +114,9 @@ class Symbol
 
   def call(valor)
     a=self
-    Proc.new{  singleton_class.send(:attr_accessor, a)
-              send("#{a}=".to_sym,valor) }
-  #  Proc.new{setea(a,valor)}
+    Proc.new { singleton_class.send(:attr_accessor, a)
+    send("#{a}=".to_sym, valor) }
+    #  Proc.new{setea(a,valor)}
   end
 end
 
