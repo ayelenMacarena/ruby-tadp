5a665e389fa68be3d74232e9f59aca956d4915c6
Rodrigo Ballestero <ballesterorj@gmail.com>
2016-05-09 21:11:54 -0300

    Punto 3

--- Tp-Ruby/src/patternMaching.rb
+++ Tp-Ruby/src/patternMaching.rb
@@ -2,54 +2,71 @@ class Object
 
   def val(valor)
     Proc.new do |parametro|
-      if valor == parametro
-        true
-      else
-        false
-      end
+      valor == parametro
     end
   end
 
   def type(tipo)
     Proc.new do |valor|
-      if (valor.class).ancestors.include? tipo
-        true
+      (valor.class).is_a? tipo
+    end
+  end
+
+  def list(lista,*bool)      ## TERMINAR - NO ANDA
+    Proc.new do |array|
+      if (bool[0]) || (bool[0] == nil)
+        lista == array
       else
-        false
+        combo = lista.zip(array)
+        combo.all? { |elem|
+          if elem[0] != nil && elem[1] != nil
+            elem[0] == elem[1]
+          end
+        }
       end
+    end
+  end
+
+  def duck(*symbol)
+    Proc.new do |objeto|
+      symbol.all? {|simbolo| (objeto.methods).include? simbolo}
     end
   end
 
   def and(*matcher)
     Proc.new do |objeto|
-      if matcher.all?{|match| match.call(objeto)}
-        self.call(objeto)
-      else
-        false
-      end
+      matcher.all?{|match| match.call(objeto)} && self.call(objeto)
     end
-
   end
 
   def or(*matcher)
     Proc.new do |objeto|
-      if matcher.any?{|match| match.call(objeto)}
-        true
-      else
-        if self.call(objeto)
-          true
-        else
-          false
-        end
-      end
+      matcher.any?{|match| match.call(objeto)} || self.call(objeto)
     end
-
   end
 
   def not
     Proc.new do |objeto|
       not self.call(objeto)
     end
+  end
+
+  def with(*matchers,&bloque)
+    Proc.new do |objeto|
+      if matchers.all? {|match| match.call(objeto)}
+        instance_exec(&bloque)
+      else
+       'noMatchea'
+      end
+    end
+  end
+
+  def otherwise(&bloque)
+    instance_exec(&bloque)
+  end
+
+  def match?(objeto,&bloque)
+
   end
 
 
